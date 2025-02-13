e857d3ea424e45e55dca3a46b9b80ca970f2fb25
Kirill Belolipetsky <archhaze24@gmail.com>
2025-02-13 10:33:21 +0300

    feat: load audio on script startup

--- script.js
+++ script.js
@@ -5,28 +5,27 @@ const bTasks = document.querySelector('#b-tasks');
 const bInfo = document.querySelector('#b-info');
 const mainText = document.querySelector('#main-text');
 const tokenCount = document.getElementById("token-count");
+let audio = new Audio("assets/tap_sound.mp3");
 
 let tokens = 1000000000000; //max =
 tokenCount.innerHTML = tokens;
 let isVibrate = true;
 let isVolume = true;
 
-function tapSound() {
-    let audio = new Audio();
-    audio.src = "assets/tap_sound.mp3";
-    audio.autoplay = true;
-}
-
 mainButton.addEventListener('touchstart', () => {
     mainButton.style.setProperty("--size-button", "42vh");
 })
 
-mainButton.addEventListener('touchend', () => {
+mainButton.addEventListener('touchend', async () => {
     mainButton.style.setProperty("--size-button", "40vh");
-    tokens+=1;
+    tokens += 1;
     tokenCount.innerHTML = tokens;
-    if(isVibrate) {navigator.vibrate(100);}
-    if(isVolume) {tapSound();}
+    if (isVibrate) {
+        navigator.vibrate(100);
+    }
+    if (isVolume) {
+        await audio.play();
+    }
 })
 
 mainText.addEventListener('click', () => {
